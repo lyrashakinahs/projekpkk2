f85e878764552ea41f954168eb48e4b0490dd972
lyrashakinahs <114457412+lyrashakinahs@users.noreply.github.com>
2022-09-27 09:46:34 +0700

    mengupload file index.php

---
+++ index.php
@@ -0,0 +1,47 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Projek PKK</title>
+</head>
+<body>`
+  <div class="nav">
+  <div class="menu-kiri">
+    <h2>WEB PKK</h2>
+  </div>
+  <div class="menu-kanan">
+    <ul>
+      <li><a href="index.php">HOME</a></li>
+      <li><a href="produk.php">PRODUK</a></li>
+      
+</ul>
+  </div>
+</div>
+<hr>
+<br>
+<h1>ANGGOTA KELOMPOK</h1>
+<table border="1">
+      <tr>
+        <th>Foto</th>
+        <th>biodata</th>
+  </body>
+    
+  <tr>
+    <td> <img src="am.jpeg" width="50"><br></td>
+    <td> NAMA : AMELIA <br> TTL : TSM,04-04-2005 <br> ALAMAT : KP.CIDAHU</td>
+  </tr>
+
+  <tr>
+    <td> <img src="IQ.png" width="50"><br></td>
+    <td> NAMA : IQLIMA <br> TTL : TSM,13-11-2005 <br> ALAMAT : JL.LEUWIANYAR</td>
+  </tr>
+
+  <tr>
+    <td> <img src="ly.jpg" width="50"><br></td>
+    <td> NAMA : LYRA SHAKINAH <br> TTL : TSM,12-12-2005 <br> ALAMAT : KP.RANGGON</td>
+  </tr>
+</html>

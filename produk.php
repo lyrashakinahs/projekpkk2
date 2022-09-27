d3c0da2a948e28a2c26bf39dd1ce9a99b685748b
lyrashakinahs <114457412+lyrashakinahs@users.noreply.github.com>
2022-09-27 09:45:55 +0700

    mengupload file produk.php

---
+++ produk.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Projek PKK</title>
+</head>
+<body>`
+  <div class="nav">
+  <div class="menu-kiri">
+    <h2>WEB PKK</h2>
+  </div>
+  <div class="menu-kanan">
+    <ul>
+      <li><a href="index.php">HOME</a></li>
+      <li><a href="index.php">PRODUK</a></li>  
+</ul>
+
+</ul>
+  </div>
+</div>
+<hr>
+<br>
+<h1>PRODUK</h1>
+<table border="1">
+      <tr>
+  </body>
+    
+  <tr>
+    <td> <img src="basreng.jpg" width="250">
+    <br><p>Nama produk : Basreng Mantul</p>
+    <p>Harga : 5k</p>
+    <p>Deskripsi : Basreng adalah cemilan khas sunda yang diiris tipis kemudian di goreng.<br>
+      Basreng dapat juga disajikan dengan taburan bumbu pedas.</p>
+      <br>
+      <button>PESAN</button>
+      </br>
+  </td>
+
+  </ul>
+  </div>
+</div>
+<br>
+<table border="1">
+        <thead>
+      <tr>
+  </body>
+    
+  <tr>
+    <td> <img src="getuk.jpg" width="250">
+    <br><p>Nama produk : Getuk</p>
+    <p>Harga : 3k</p>
+    <p>Deskripsi : Getuk adalah makanan ringan yang terbuat dari bahan singkong.<br>
+      Dengan dicampur gula merah dan ditaburi parutan kelapa.</p>
+      <br>
+      <button>PESAN</button>
+      </br>
+  </td>
+
+  </ul>
+  </div>
+</div>
+<br>
+<table border="1">
+        <thead>
+      <tr>
+  </body>
+    
+  <tr>
+    <td><img src="telur.jpeg" width="250">
+    <br><p>Nama produk : Telur Gulung</p>
+    <p>Harga : 1k</p>
+    <p>Deskripsi : Telur gulung adalah makanan Tradisional dan merupakan variasi dari telur goreng,</p>
+    yang di mana sebuah telur akan digoreng,</p>
+    kemudian digulung menggunakan sebuah tusukan yang biasanya di buat dari kayu pohon Bambu. </p>
+    <br>
+    <button>PESAN</button>
+    </br>
+  </td>
+</tr>
